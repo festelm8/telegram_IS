66e5f5d168c55867a91b0ac680441e08953d2798
Sergey Nikulin <s.nikulin@pr-solution.ru>
2018-01-22 13:43:47 +0300

    test app

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from shutil import rmtree
 from pathlib import Path
 import datetime
 import os
+import telebot
 
 from src import create_app
 from src.db import db, rq
@@ -24,6 +25,34 @@ def run():
         threaded=True
     )
 
+def cock():
+    bot = telebot.TeleBot('516834738:AAFiBE5c-0TDGBQUBY2OvDvHDgh0UNBZrOU')
+
+    @bot.message_handler(commands=['start', 'help'])
+    def send_welcome(message):
+        # print(message.from_user.first_name)
+        # bot.reply_to(message, u"Hello, welcome to this bot!")
+        # bot.send_message(chat_id=message.from_user.id, text='cock')
+        # markup = telebot.types.ReplyKeyboardRemove()
+        markup = telebot.types.InlineKeyboardMarkup(row_width=6)
+        gobtn1 = telebot.types.InlineKeyboardButton(text='Го', callback_data='say_ok')
+        gobtn2 = telebot.types.InlineKeyboardButton(text='Го', callback_data='say_ok')
+        gobtn3 = telebot.types.InlineKeyboardButton(text='Го', callback_data='say_ok')
+        markup.row(gobtn1)
+        markup.row(gobtn2, gobtn3)
+        bot.send_message(chat_id=message.from_user.id, text='Шо го? Камон коуч бой летс ду зис', reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: call.data == 'say_ok')
+    def test_callback(call):
+        bot.send_message(call.from_user.id, text='найс найс')
+
+    # @bot.message_handler(regexp='Похналэ')
+    # def alo(message):
+    #     print('got it')
+
+    bot.polling()
+    # print(bot.get_updates())
+
 
 tests_command = Command(run_unit_tests)
 tests_command.option_list[0].kwargs['nargs'] = '*'
@@ -33,6 +62,7 @@ manager.add_command("run", Command(run))
 manager.add_command('rq', RQManager(rq))
 manager.add_command("test", tests_command)
 manager.add_command("shell", Shell())
+manager.add_command("cock", Command(cock))
 
 if __name__ == '__main__':
     manager.run()
