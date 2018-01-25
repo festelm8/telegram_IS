3a01d480420aeae7ebf98ef4b6d308cd7fdc77da
Sergey Nikulin <s.nikulin@damprodam.ru>
2018-01-26 01:12:49 +0300

    done with database

--- src/db/models/course_group.py
+++ src/db/models/course_group.py
@@ -10,6 +10,6 @@ class CourseGroup(db.Model):
     id = db.Column(UUID,
                    server_default=db.text('gen_random_uuid()'),
                    primary_key=True)
-    gid = db.Column(db.INTEGER, default=0)
+    gid = db.Column(db.VARCHAR(30))
     course_number_id = db.Column(UUID, db.ForeignKey('course_numbers.id'))
     course_number = db.relationship('CourseNumber')
